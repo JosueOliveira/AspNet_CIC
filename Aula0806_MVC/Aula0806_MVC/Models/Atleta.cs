3c61c15db8e7f21a6f552696b7629ee3e47dd517
Josué <josueoloveira29@gmail.com>
2017-06-08 23:13:48 -0300

    Criado models controllers Views

---
+++ Aula0806_MVC/Aula0806_MVC/Models/Atleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aula0806_MVC.Models
+{
+    public class Atleta
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Modalidade { get; set; }
+        public int Medalhas { get; set; }
+    }
+}
