285f3020a847c3727f85173171bb3913c0bca3b8
Josué <josueoloveira29@gmail.com>
2017-06-13 21:16:49 -0300

    Criando ControllerView e index Categorias

---
+++ Aula1306_MVC/Aula1306_MVC/Controllers/CategoriasController.cs
@@ -0,0 +1,32 @@
+﻿using Aula1306_MVC.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Aula1306_MVC.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+
+            //usando ViewBag
+            //List<string> categorias = new List<string>();
+            //categorias.Add("Carros");
+            //categorias.Add("Motos");
+            //categorias.Add("Barcos");
+            //categorias.Add("Caminhoes");
+            ////viewBag envia informações para View
+            //ViewBag.ListaCategorias = categorias;
+
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(new Categoria() { Nome = "Carros" });
+            categorias.Add(new Categoria() { Nome = "Motos" });
+            categorias.Add(new Categoria() { Nome = "Barcos" });
+            categorias.Add(new Categoria() { Nome = "Caminhoes" });
+
+            return View(categorias);
+        }
+    }
+}
---
+++ Aula1306_MVC/Aula1306_MVC/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula1306_MVC.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
